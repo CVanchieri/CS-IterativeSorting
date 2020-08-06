e65553c09dde9a8f0b2ea5d42fbcd0aeb6037c90
Charles Vanchieri <32582544+CVanchieri@users.noreply.github.com>
2020-08-05 20:20:05 -0700

    1st part of assignment.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,46 +1,58 @@
 # TO-DO: Complete the selection_sort() function below
-def selection_sort(arr):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7] # create new arrray, length of 10
+"""
+1. INdex = 0 starting point
+2. For loop all indices 'except' the last index
+    a. Loop through values on right-hand-side of current index and find the smallest value
+    b. Swap the values at current index with the smallest values found in above loop
+Selection sort segments the list into two parts, 1)the left most is sorted 2)the right is unsorted
+1. Search through list for smallest element
+2. Once found, swap it with the first value, now sorted
+3. Find smallest value of remainig items, swap with second value
+i = # of values sorted, as i increases we check less items so j decreases
+j = iterate over unsorted items, which are
+i 0, check j 1-9 items
+i 1, check j 2-9 items
+"""
+def selection_sort(arr): # function for selection_sort operation
+    for i in range(0, len(arr)-1): # for loop through n-1 elements
+        smallest_index = i # temp variable to store position of min value, assumes 1st value to be min of unsorted array
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
-
-
-        # TO-DO: swap
-        # Your code here
-
+        for j in range(i + 1, len(arr)):
+            print(f'i: {i}, j:{j}')
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i] # swap
+        print("""---
+PASS COMPLETE
+---""")
     return arr
 
+print(selection_sort(arr1)) # print the function, use on the arr1 array
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort(arr):
-    # Your code here
-
-
+"""
+1. Loop through the array
+    a. Compare each value to its neighbor
+    b. If values in wrong position(relative to each other, swap them)
+2. If no swaps performed, stop. Else, go back to the value at index 0 and repeat step 1.
+"""
+def bubble_sort(arr): # function for bubble_sort operation
+    for i in range(0, len(arr)): # for loop over ever item in the array
+        for j in range(0, len(arr) - 1): # for every item in array, compare it to it's neighbhor
+            print(arr, arr[j], arr[j+1])
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j+1] = temp # swap
+        print("""---
+PASS COMPLETE
+---""")
     return arr
 
-'''
-STRETCH: implement the Counting Sort function below
+print(bubble_sort(arr1)) # print the function, use on the arr1 array
 
-Counting sort is a sorting algorithm that works on a set of data where
-we specifically know the maximum value that can exist in that set of
-data. The idea behind this algorithm then is that we can create "buckets"
-from 0 up to the max value. This is most easily done by initializing an
-array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
-
-Each buckets[i] then is responsible for keeping track of how many times 
-we've seen `i` in the input set of data as we iterate through it.
-Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
-
-What is the time and space complexity of the counting sort algorithm?
-'''
-def counting_sort(arr, maximum=None):
-    # Your code here
-
+# STRETCH: implement the Count Sort function below
+def count_sort(arr, maximum=-1):
 
     return arr
