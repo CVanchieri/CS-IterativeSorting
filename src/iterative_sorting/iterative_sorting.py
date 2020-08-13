005c46c4012f2722a9cfce59ef72a0c2eb9c2238
Charles Vanchieri <32582544+CVanchieri@users.noreply.github.com>
2020-08-12 17:59:42 -0700

    updated notes on assignment

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,6 +1,7 @@
 # TO-DO: Complete the selection_sort() function below
 arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7] # create new arrray, length of 10
 """
+Notes:
 1. INdex = 0 starting point
 2. For loop all indices 'except' the last index
     a. Loop through values on right-hand-side of current index and find the smallest value
@@ -14,6 +15,15 @@ j = iterate over unsorted items, which are
 i 0, check j 1-9 items
 i 1, check j 2-9 items
 """
+'''
+Selection Sort:
+The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order)
+from unsorted part and putting it at the beginning. The algorithm maintains two subarrays in a given array.
+1) The subarray which is already sorted.
+2) Remaining subarray which is unsorted.
+In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is
+picked and moved to the sorted subarray.
+'''
 def selection_sort(arr): # function for selection_sort operation
     for i in range(0, len(arr)-1): # for loop through n-1 elements
         smallest_index = i # temp variable to store position of min value, assumes 1st value to be min of unsorted array
@@ -29,14 +39,39 @@ PASS COMPLETE
     return arr
 
 print(selection_sort(arr1)) # print the function, use on the arr1 array
+'''
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
+    index_length =  range(0, len(arr) - 1)
 
+    # loop through n-1 elements
+    for i in index_length:
+        cur_index = i
+
+        for j in range( i + 1 , len(arr)):
+            if arr[j] < arr[cur_index]:
+                cur_index = j
+
+        if cur_index != i:
+            arr[cur_index], arr[i] = arr[i], arr[cur_index]
+
+    return arr
+
+print(selection_sort(arr1)) # print the function, use on the arr1 array
+'''
 # TO-DO:  implement the Bubble Sort function below
 """
+Notes:
 1. Loop through the array
     a. Compare each value to its neighbor
-    b. If values in wrong position(relative to each other, swap them)
+    b. If values in wrong position (relative to each other, swap them)
 2. If no swaps performed, stop. Else, go back to the value at index 0 and repeat step 1.
 """
+'''
+Bubble Sort:
+Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements
+if they are in wrong order.
+'''
 def bubble_sort(arr): # function for bubble_sort operation
     for i in range(0, len(arr)): # for loop over ever item in the array
         for j in range(0, len(arr) - 1): # for every item in array, compare it to it's neighbhor
@@ -51,8 +86,29 @@ PASS COMPLETE
     return arr
 
 print(bubble_sort(arr1)) # print the function, use on the arr1 array
+'''
+# TO-DO:  implement the Bubble Sort function below
+def bubble_sort(arr):
+    index_length = len(arr) -1
+    sorted = False
+
+    while not sorted:
+        sorted = True
+        for i in range(0, index_length):
+            if arr[i] > arr[i+1]:
+                sorted = False
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+    return arr
+
+print(bubble_sort(arr1)) # print the function, use on the arr1 array
+'''
 
 # STRETCH: implement the Count Sort function below
+'''
+Counting sort is a sorting technique based on keys between a specific range. It works by counting the
+number of objects having distinct key values (kind of hashing). Then doing some arithmetic to calculate
+the position of each object in the output sequence.
+'''
 def count_sort(arr, maximum=-1):
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,45 @@
-def linear_search(arr, target):
-    # Your code here
-
-
-    return -1   # not found
-
+'''
+Iterative:
+A program is called recursive when an entity calls itself. A program is call iterative when there is a loop (or repetition).
+'''
+# Write a linear search approach
+'''
+Linear Search:
+A simple approach is to do linear search, i.e
+Start from the leftmost element of arr[] and one by one compare x with each element of arr[]
+    1. If x matches with an element, return the index.
+    2. If x doesn’t match with any of elements, return -1.
+'''
+def linear_search(arr, target): # function to implement linear search
+    for i in range(len(arr)): # for the i value in the range of the length of the array
+        if arr[i] == target: # if the arr index value for i is equal to the target
+            return i # return the value
+    return -1   # return not found
 
 # Write an iterative implementation of Binary Search
-def binary_search(arr, target):
+'''
+# Binary Search:
+This search algorithm takes advantage of a collection of elements that is already sorted by ignoring
+half of the elements after just one comparison.
+    1. Compare x with the middle element.
+    2. If x matches with the middle element, we return the mid index.
+    3. Else if x is greater than the mid element, then x can only lie in the right (greater) half subarray
+    after the mid element. Then we apply the algorithm again for the right half.
+    4. Else if x is smaller, the target x must lie in the left (lower) half. So we apply the algorithm for
+    the left half.
+'''
+def binary_search(arr, target): # function to implement binary search
+    start = 0 # set the star to 0
+    stop = len(arr) - 1 # set the stop to the length of the arr minus 1
 
-    # Your code here
-
+    while start <= stop: # while the start value is less than the stop value
+        midpoint = start + (stop - start)//2 # set the midpoint to start value and (stop value minus start value) divided by 2
+        midpoint_val = arr[midpoint] # set the midpoint to the arr midpoint
+        if midpoint_val == target: # if the midpoint_val is equal to the target
+            return midpoint # return the midpoint value
+        elif target <midpoint_val: # else if the target is less than the midpoint_val
+            stop = midpoint - 1 # set stop to the midpoint minus 1
+        else: # else
+            start = midpoint + 1 # set the start value to the midpoint plus 1
 
     return -1  # not found
